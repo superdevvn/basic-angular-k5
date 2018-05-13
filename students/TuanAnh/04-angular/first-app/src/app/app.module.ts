0b12edb2e09e7c9fe483096042e807cac6312a9f
USER\WIN <doantuananh2095@gmail.com>
2018-05-13 12:01:05 +0700

    04angular

---
+++ students/TuanAnh/04-angular/first-app/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstPageComponent,
+    SecondPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ students/TuanAnh/04-angular/first-app/src/app/first-page/first-page.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-page',
+  templateUrl: './first-page.component.html',
+  styleUrls: ['./first-page.component.css']
+})
+export class FirstPageComponent implements OnInit {
+  username :string;
+  password : string;
+  rememberMe :boolean;
+  role : string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  click(){
+    console.log('Clicked');
+    console.log(this.username);
+    console.log(this.password);
+    console.log(this.rememberMe);
+    console.log(this.role);
+  }
+}
