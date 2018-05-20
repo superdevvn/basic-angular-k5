36192a30e2feefa081f5ff6bfd110fc18733959d
mducptit <33183277+mducptit@users.noreply.github.com>
2018-05-20 11:02:39 +0700

    06-SERVICE

---
+++ students/VoMinhDuc/06-SERVICE/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { RoleListComponent } from './role-list/role-list.component';
+import { RoleDetailComponent } from './role-detail/role-detail.component';
+import { LoginComponent } from './login/login.component';
+import { UtilityService } from './services/utility.service';
+import { ApiService } from './services/api.service';
+import { HttpModule } from '@angular/http';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  {
+    path: 'main', component: MainComponent, children: [
+      { path: 'role-list', component: RoleListComponent },
+      { path: 'role-detail', component: RoleDetailComponent }
+    ]
+  },
+  { path: 'login', component: LoginComponent }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    RoleListComponent,
+    RoleDetailComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [
+    UtilityService,
+    ApiService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ students/VoMinhDuc/06-SERVICE/src/app/services/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: Http) { }
+  post(url: string, data: any){
+    this.http.post(url, data).toPromise().then(res=>{
+      console.log(res.json());
+    }).catch(err=>{
+      console.log(err);
+    });
+  }
+}
